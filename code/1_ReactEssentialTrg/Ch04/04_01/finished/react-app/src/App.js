d85660d497190ce9aa001560ba26b3ee7f0caf86
Lucas Leow <leowjianhoong@gmail.com>
2023-06-01 21:11:35 +0800

    Completed Module 5

--- code/1_ReactEssentialTrg/Ch04/04_01/finished/react-app/src/App.js
+++ code/1_ReactEssentialTrg/Ch04/04_01/finished/react-app/src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
-import {  useReducer  } from "react";
-		
-function App( {}) {
+import { useState } from "react";
 
-  const [checked, setChecked] = useReducer(
-    (checked)=> !checked,
-    false
-    );
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  return [
+    {value, onChange: e => setValue(e.target.value)},
+    () => setValue(initialValue) // reset value
+  ]
+}
+
+function App() {
+
+  const [test, changeTest] = useState();
+  console.log(test);
+  const [titleProp, resetTitle] = useInput(""); // initial state is empty
+  const[hexProp, resetHex] = useInput("#000000");
+
+const submit = (event) => {
+  event.preventDefault(); // prevent page from refreshing
+  alert(`Selected Color: ${titleProp.value} \n Selected Hex: ${hexProp.value}`)
+ resetTitle();
+  resetHex();
+
+};
+
   return (
-    <div className="App">
-      <input 
-        type="checkbox" 
-        value={checked} 
-        onChange={setChecked}
-      />
-      <label>{checked? "checked":"not checked"}</label>
-    </div>
-  )
-}	
+
+    <form onSubmit = {submit}>
+      <input {...titleProp} placeholder="Color title..."/>
+      <input {...hexProp} type="color"/>
+      <button>ADD</button>
+      
+    </form>
+  );
+}
 
 export default App;
